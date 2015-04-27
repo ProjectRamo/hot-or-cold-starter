cd5ef634f2e389af6fb0f57aee45d6d998c62ef1
ProjectRamo <ohaneef@SMV-OH-LT01.smallventuresusa.com>
2015-04-27 14:52:37 -0400

    Almost done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 
+   var target = Math.round(Math.random()*100);
+   var count = 0;
+   var guess = $('#userGuess').val();
+   var gap = 0;
+
+function newGame(){
+	alert("Let's just say a new game started");
+	target = Math.round(Math.random()*100);
+  $('#guessList').empty();
+  count = 0;
+  $('#count').text(count+"")
+  $("#feedback").text("New Game");
+  $('#userGuess').val('');
+}
+
 $(document).ready(function(){
-	
+  guess = $('#userGuess').val();
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +26,42 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	$("a.new").click(function(){
+  		newGame();
+  	});
+
+    $('#guessButton').click(function(){
+      count += 1;
+      guess = $('#userGuess').val();
+      gap = guess-target;
+      switch(gap) {
+        case (gap < -10):
+          feedback = "Cold, higher"
+          break;
+        case (gap < -20):
+          feedback = "Ice cold bra, higher"
+          break;
+        case (gap > 10):
+          feedback = "hot, lower"
+          break;
+        case (gap > 20):
+          feedback = "Too hot bra, lower"
+          break;
+        case (gap == 0):
+          break;
+        default:
+          feedback = "Something is amiss!"
+      }
+      if (guess==target) {
+        $("#feedback").text(">> YOU WON! <<");
+        alert ("You won! Yaaay");
+        newGame();
+      }
+      $("#feedback").text(feedback);
+      $('#guessList').append("<li>"+guess+"</li>");
+      $('#count').text(count+"")
+  });
 
 });
 
